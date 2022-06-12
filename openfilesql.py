5f5dd0c5729bc90a5063ae2b4c1a25d2749f6470
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2022-06-12 15:26:07 -0400

    Update openfilesql.py

--- openfilesql.py
+++ openfilesql.py
@@ -51,3 +51,14 @@ with engine.connect() as con:
 
 # Print head of DataFrame df
 print(df.head())
+
+# FASTER METHOD
+
+import pandas as pd
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000', engine)
+
+# Print head of DataFrame
+print(df.head())
