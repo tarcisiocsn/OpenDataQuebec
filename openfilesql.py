32f758aff613f6d3aaa7d249b38562b43979dc0f
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2022-06-12 14:39:13 -0400

    Create openfilesql.py

---
+++ openfilesql.py
@@ -0,0 +1,14 @@
+# Create engine: engine
+import pandas as pd
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee ORDER BY BirthDate')
+    df = pd.DataFrame(rs.fetchall())
+    # Set the DataFrame's column names
+    df.columns = rs.keys()
+
+
+# Print head of DataFrame
+print(df.head())
