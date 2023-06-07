853fb5e544212b075331062a256e2ce8c1fc2bb2
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2023-06-07 11:18:07 -0400

    Update telecharger_fichiers.py

--- QuebecTrafficVolume/telecharger_fichiers.py
+++ QuebecTrafficVolume/telecharger_fichiers.py
@@ -78,9 +78,18 @@ import requests
 import os
 
 # Create a folder for the downloaded files
-download_folder = '/path/debit_circulation_quebec/'
-if not os.path.exists(download_folder):
-    os.makedirs(download_folder)
+# Specify the relative path to the folder
+relative_folder_path = 'debit_circulation_quebec'
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Create the full path for the new folder
+new_folder_path = os.path.join(current_dir, relative_folder_path)
+
+# Create the new folder
+if not os.path.exists(new_folder_path):
+    os.makedirs(new_folder_path)
 
 # Function to download a file from a URL
 def download_file(url, folder):
